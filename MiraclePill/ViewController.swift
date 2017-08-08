6361a447fcbc36a906d0d40979fa7159bbf7b507
Scott <spelo3@gmail.com>
2017-08-08 15:50:43 -0700

    first commit

--- MiraclePill/ViewController.swift
+++ MiraclePill/ViewController.swift
@@ -8,11 +8,22 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
+    
+    @IBOutlet weak var statePicker: UIPickerView!
+    @IBOutlet weak var statePickerBtn: UIButton!
+    @IBOutlet weak var countryLabel: UILabel!
+    @IBOutlet weak var countryInput: UITextField!
+    @IBOutlet weak var zipLabel: UILabel!
+    @IBOutlet weak var zipInput: UITextField!
+    @IBOutlet weak var successView: UIImageView!
+    
+    let states = ["California", "Arizona", "Oregon", "Washington"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        statePicker.dataSource = self
+        statePicker.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +31,40 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func statePickerPressed(_ sender: Any) {
+        statePicker.isHidden = false
+        countryLabel.isHidden = true
+        countryInput.isHidden = true
+        zipLabel.isHidden = true
+        zipInput.isHidden = true
+    }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return states.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return states[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        statePickerBtn.setTitle(states[row], for: UIControlState.normal)
+        statePicker.isHidden = true
+        countryLabel.isHidden = false
+        countryInput.isHidden = false
+        zipLabel.isHidden = false
+        zipInput.isHidden = false
+    }
 
+    @IBAction func buyNowButtonPressed(_ sender: Any) {
+        for view in self.view.subviews as [UIView] {
+            view.isHidden = true
+        }
+        successView.isHidden = false
+    }
 }
 
